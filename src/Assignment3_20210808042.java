38b71b964f66a4e551117195b8630adde59c5783
Muslum Agah <muslumagah7@gmail.com>
2023-11-29 20:16:57 +0300

    Second

--- src/Assignment3_20210808042.java
+++ src/Assignment3_20210808042.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Assignment3_20210808042 {
     public static void main(String[] args) {
-       
+        Scanner inp = new Scanner(System.in);
+        String[] list = {"0 - Enter All Grades", "1 - Display grade information",
+                "2 - Change a single grade", "Q - to Quit"};
+        menu(inp,list);
     }
 
+    public static int menu(Scanner inp, String[] list) {
+        do{
+            for (int i = 0; i < list.length; i++) {
+                System.out.println(list[i]);
+            }
+        }while(inp.nextInt()>=3);
+        return 1;
+    }
+    public static void calculateGrade(int category, int quantity, int weight){
 
+    }
 }
