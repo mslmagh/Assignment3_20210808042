d008d58c1cdd88abe92341adbcc31bd160b7a0f7
Muslum Agah <muslumagah7@gmail.com>
2023-11-30 16:55:00 +0300

    Update Assignment3_20210808042.java

--- src/Assignment3_20210808042.java
+++ src/Assignment3_20210808042.java
@@ -2,11 +2,22 @@ import java.util.Scanner;
 
 public class Assignment3_20210808042 {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         Scanner inp = new Scanner(System.in);
         String[] list = {"0 - Enter All Grades", "1 - Display grade information",
                 "2 - Change a single grade", "Q - to Quit"};
         menu(inp,list);
         System.out.println();
+=======
+        System.out.println("Welcome to our university grade system.");
+        String[] categories = {"QUiZ", "HomEwOrk", "Midterm exam", "Final exam"};
+        formatCategoryName(categories);
+        int[] quantities = {4, 2, 2, 1}; // initialized with 0, update based on user input
+        int[] weights = new int[]{10, 20, 30, 40}; // example weights, update based on user input
+        calculateGrade(categories, quantities, weights);
+        System.out.println("a");
+        
+>>>>>>> Stashed changes
     }
 
     public static int menu(Scanner inp, String[] list) {
